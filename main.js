7ac2e5ea0e36caf42806fe404e50f9efb03eb7a9
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-29 04:44:45 +0500

    project011

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let weightInkg = 70; //your current weight in kgs
+let heightInmeter = 1.75; //your current height in meter
+let bmi = weightInkg / (heightInmeter * heightInmeter); // formula of bmi calcultor
+console.log(`Your BMI is ${bmi}`); //result
---
+++ main.ts
@@ -0,0 +1,8 @@
+let weightInkg =70; //your current weight in kgs
+
+let heightInmeter =1.75; //your current height in meter
+
+let bmi = weightInkg / (heightInmeter * heightInmeter); // formula of bmi calcultor
+
+console.log(`Your BMI is ${bmi}`); //result
+
